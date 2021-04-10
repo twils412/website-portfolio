be3b6164c6174eb420c555a5b5e6eb78d2e53c8c
Tyler Wilson <twils412@ufl.edu>
2021-04-10 12:33:16 -0400

    trying out internal CSS instead of external

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,4 +1,65 @@
-  <link type="text/css" rel="stylesheet" href="../css/styles.css"/>
+<style type="text/css">
+
+@font-face{
+  font-family: 'Acme', sans-serif;
+  font-family: 'Josefin Sans', sans-serif;}
+
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+p {
+  font-family: 'Josefin Sans', sans-serif;
+  line-height: 25px;
+  padding: 10px;
+  font-size: 17px;
+}
+
+h1 {
+  color: white;
+  font-family: 'Acme', sans-serif;
+  text-align: center;
+  font-size: 40px;
+  margin: 12px 0 30px;
+
+  header {
+    background-color: #111111;
+    padding: 10px;
+    width: 100vw;
+    height: 18%;
+    z-index: 999;
+    position: fixed;
+  }
+
+  .content {
+    background-color: #333333;
+    color: white;
+    margin: 0 auto;
+    max-width: 78%;
+    padding: 10px;
+    min-height: 100vh;
+    position: relative;
+  }
+
+  footer {
+    background-color: #111111;
+    color: white;
+    text-align: center;
+    z-index: 5;
+    padding: 25px;
+    font-family: 'Acme', sans-serif;
+  }
+
+  .footer-style {
+    color: #7c5da7;
+  }
+
+}
+
+</style>
 
 <header>
 
